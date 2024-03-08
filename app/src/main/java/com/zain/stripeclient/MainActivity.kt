2d1e7a3a51a0c530c03294d9e276d3b22c0f22b5
zainaraza43 <urboyzain@gmail.com>
2024-03-08 00:00:27 -0500

    added security and a functioning demo

--- app/src/main/java/com/zain/stripeclient/MainActivity.kt
+++ app/src/main/java/com/zain/stripeclient/MainActivity.kt
@@ -1,53 +1,98 @@
 package com.zain.stripeclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
 import com.stripe.android.paymentsheet.rememberPaymentSheet
-import com.zain.stripeclient.ui.theme.StripeClientTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val paymentSheet = rememberPaymentSheet(::onPaymentSheetResult)
-            StripeClientTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val context = LocalContext.current
+            var customerConfig by remember { mutableStateOf<PaymentSheet.CustomerConfiguration?>(null) }
+            var paymentIntentClientSecret by remember { mutableStateOf<String?>(null) }
+
+            LaunchedEffect(context) {
+                "http://192.168.2.198:5000/payment-sheet".httpPost().responseJson { _, _, result ->
+                    if (result is Result.Success) {
+                        val responseJson = result.get().obj()
+                        Log.i(TAG, "result=$responseJson")
+                        paymentIntentClientSecret = responseJson.getString("paymentIntent")
+                        customerConfig = PaymentSheet.CustomerConfiguration(
+                            responseJson.getString("customer"),
+                            responseJson.getString("ephemeralKey")
+                        )
+                        val publishableKey = responseJson.getString("publishableKey")
+                        PaymentConfiguration.init(context, publishableKey)
+                    }
                 }
+            }
+
+            Button(
+                onClick = {
+                    val currentConfig = customerConfig
+                    val currentClientSecret = paymentIntentClientSecret
+
+                    if (currentConfig != null && currentClientSecret != null) {
+                        presentPaymentSheet(paymentSheet, currentConfig, currentClientSecret)
+                    }
+                }
+            ) {
+                Text("Checkout")
             }
         }
     }
 }
 
-private fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
-
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name",
-        modifier = modifier
+private fun presentPaymentSheet(
+    paymentSheet: PaymentSheet,
+    customerConfig: PaymentSheet.CustomerConfiguration,
+    paymentIntentClientSecret: String
+) {
+    paymentSheet.presentWithPaymentIntent(
+        paymentIntentClientSecret,
+        PaymentSheet.Configuration(
+            merchantDisplayName = "My merchant name",
+            customer = customerConfig,
+            // Set `allowsDelayedPaymentMethods` to true if your business handles
+            // delayed notification payment methods like US bank accounts.
+            allowsDelayedPaymentMethods = true
+        )
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StripeClientTheme {
-        Greeting("Android")
+private fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
+    when(paymentSheetResult) {
+        is PaymentSheetResult.Canceled -> {
+            print("Canceled")
+        }
+        is PaymentSheetResult.Failed -> {
+            print("Error: ${paymentSheetResult.error}")
+        }
+        is PaymentSheetResult.Completed -> {
+            // Display for example, an order confirmation screen
+            print("Completed")
+        }
     }
 }
